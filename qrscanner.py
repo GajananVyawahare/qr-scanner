4e43668214f63c597485733c3925b0795223939f
GajananVyawahare <your.gajananvyawahare610@gmail.com>
2025-09-24 00:43:55 +0530

    Version 2

--- qrscanner.py
+++ qrscanner.py
@@ -2,8 +2,8 @@ import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-myqr = qrcode.make("https://www.instagram.com/pritam_official_1137?igsh=aGhueTBubXlyb3N1&utm_source=qr")
-myqr1 = qrcode.make("https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTI1NjkzMzQyMTE2NDUw?story_media_id=3718701748895382397&igsh=MWNzaDB1dzl5aHFyMw==")
+myqr = qrcode.make("gajananvyawahare610@gmail.com")
+myqr1 = qrcode.make("https://github.com/GajananVyawahare/qr-scanner.git")
 
 
 myqr.save("myqr.png",scale = 8)
